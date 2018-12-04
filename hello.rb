17e38795281abb0714ef2f8f9dfc278a88a6fccb
Greg Witt <36649901+GoodGuyGregory@users.noreply.github.com>
2018-12-03 22:04:50 -0500

    adds initial commit in Ruby Language
    Add Hello World program in the Ruby Language

---
+++ hello.rb
@@ -0,0 +1,8 @@
+## Hello world from Ruby
+## compile the program by typing ruby <filename>.rb
+## Ruby uses Bundler- allowing for packages and libraries
+# Ruby Sass 
+# Puppet and Chef - Sets up entire operating systems with easy
+# Rails- web framework allow user database manipulation
+
+puts "Hello from Ruby!"
