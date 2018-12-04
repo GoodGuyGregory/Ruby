5872a483e7cbabfc9342947b133e45bdf67c73f4
Greg Witt <36649901+GoodGuyGregory@users.noreply.github.com>
2018-12-03 22:06:30 -0500

    adds initial files for the Widgets project
    Initial start5 of the widgets project in Ruby lang

---
+++ widgets.rb
@@ -0,0 +1,10 @@
+#Display Welcome Message
+
+# puts is called Put-S it is a function which is called put string
+puts "Hello, Welcome to the Widgets Store!"
+
+#Ask for Quantity
+
+# Calculate a running total
+
+# Discount larger  quantity orders
