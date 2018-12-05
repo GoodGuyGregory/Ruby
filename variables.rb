01c637d04059685aed30896fb37b0770712de499
Greg Witt <36649901+GoodGuyGregory@users.noreply.github.com>
2018-12-04 22:27:51 -0500

    Adds additional files for Basics Task

---
+++ variables.rb
@@ -0,0 +1,12 @@
+number = 4
+greeting = "hello"
+
+#display variables
+puts number
+puts greeting
+
+#display number variable
+puts number + 2 
+#change the value for number
+puts number = 10
+
--- widgets.rb
+++ widgets.rb
@@ -4,7 +4,16 @@
 puts "Hello, Welcome to the Widgets Store!"
 
 #Ask for Quantity
+def ask(question)
+  print question
+  #takes what the user types
+  answer = gets
+  puts answer
+end
+# store the new quantity the customer desires for us to manipulate 
+answer = ask("How many Widgets are you ordering? ")
 
+puts answer
 # Calculate a running total
 
 # Discount larger  quantity orders
