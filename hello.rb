5dac11356d7e3a9c5fadf98da7f035e041e1dce8
GoodGuyGregory <greg.witt625@gmail.com>
2018-12-18 18:55:17 -0500

    removes hello

--- hello.rb
+++
@@ -1,8 +0,0 @@
-## Hello world from Ruby
-## compile the program by typing ruby <filename>.rb
-## Ruby uses Bundler- allowing for packages and libraries
-# Ruby Sass 
-# Puppet and Chef - Sets up entire operating systems with easy
-# Rails- web framework allow user database manipulation
-# Prints Hello from Ruby!
-puts "Hello from Ruby!"
