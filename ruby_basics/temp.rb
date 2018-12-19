97f568d881a2bd83e9d7d1e1fdbd607607ca44bd
GoodGuyGregory <greg.witt625@gmail.com>
2018-12-19 15:06:32 -0500

    adds changes to temp

--- ruby_basics/temp.rb
+++ ruby_basics/temp.rb
@@ -10,6 +10,23 @@ def count_to_three
   puts 3
 end
 
-count_to_three
-wait 45
+# elsif and else
+# Basics conitional 
+if_condition = false
+if if_condition
+	puts "if clause"
+else
+	puts "else clause"
+end
+# elseif
+if_condition = true
+elsif_condition = true
+if if_condition
+	puts "if clause"
+elsif elsif_condition
+	puts "elsif clause"
+else 
+	puts "else clause"
+end
+
 
