355bea8903bf8adc37ad22a7b37b4a4140d27ca3
GoodGuyGregory <greg.witt625@gmail.com>
2018-12-19 15:06:59 -0500

    adds final changes

--- ruby_basics/widgets.rb
+++ ruby_basics/widgets.rb
@@ -1,4 +1,4 @@
-#Display Welcome Message
+ #Display Welcome Message
 
 # puts is called Put-S it is a function which is called put string
 puts "Hello, Welcome to the Widgets Store!"
@@ -12,13 +12,33 @@ def ask(question)
   gets.chomp
 end
 
+# Calculate a running total
+# Discount larger  quantity orders
+# 100+ $8.00
+# 50+ $9.00
+# others should be for $0.00
+def price(quantity)
+	if quantity >= 100 
+		price_per_item = 8
+	
+	elsif	quantity >= 50
+		price_per_item = 9
+	
+	else quantity < 50
+		price_per_item = 10
+	end
+	quantity * price_per_item
+end
+
 # store the new quantity the customer desires for us to manipulate 
 answer = ask("How many Widgets are you ordering?")
+number = answer.to_i
+# Calculate the runninng total through the price method
+total = price(number)
 
 #this shows the string interpolation feature of ruby
-puts "You entered #{answer} widgets"
-p answer
-# Calculate a running total
-total = answer.to_i * 10
-p total
-# Discount larger  quantity orders
+# puts "You entered #{answer} widgets"
+
+# interpolates answer by 10 to display total
+puts "For #{number} widgets, your total is: $#{total}"
+
