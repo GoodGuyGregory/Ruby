321ca8c6d7ec0a82a6fd29f2245464da9f4705a8
GoodGuyGregory <greg.witt625@gmail.com>
2019-01-16 12:14:43 -0500

    explains lines and adds first passing test case

--- ruby_loops/contact_list.rb
+++ ruby_loops/contact_list.rb
@@ -3,8 +3,17 @@ contact_list = []
 
 #creates a method to ask the user questions regarding contacts
 def ask(question , kind="string")
+  # prints the question to the screen
   print question + " "
-  answer = get.chomp
+  # gets the answer from the user
+  answer = gets.chomp
+  # if the question is a phone number cast to integer
   answer = answer.to_i if kind == "number"
   return answer
 end
+
+# Test:
+# answer = ask("What is your name? ")
+# puts answer
+
+
