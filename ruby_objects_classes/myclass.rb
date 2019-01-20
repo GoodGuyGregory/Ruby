f63f14ec2f846a8622c3037ae6e66f6703702b71
GoodGuyGregory <greg.witt625@gmail.com>
2019-01-20 12:57:11 -0500

    inital commit

---
+++ ruby_objects_classes/myclass.rb
@@ -0,0 +1,11 @@
+#first class
+# creating classes in ruby is known as instatiation
+
+class MyClass
+  def initialize
+    puts "this is the initialize method."
+  end
+end
+
+#instantiation of the class object
+MyClass.new
